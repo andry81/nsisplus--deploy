abc5631243a5854db97b1afaf06a448a08c5cfae
andry81 <andry@inbox.ru>
2017-06-22 23:07:38 +0000

    svn to git sync scripts
    git-svn-id: svn+ssh://svn.code.sf.net/p/nsisplus/deploy/trunk@2 493e4a90-db5d-4955-a802-ededfdaf30c0

---
+++ NsisSetup/git/NsisSetupSamples_git.sync_all.bat
@@ -0,0 +1,26 @@
+@echo off
+
+setlocal
+
+if "%NEST_LVL%" == "" set NEST_LVL=0
+
+set /A NEST_LVL+=1
+
+pushd "%~dp0NsisSetupSamples_git" && (
+  call :CMD git pull origin master || ( popd & goto EXIT )
+  call :CMD git svn fetch || ( popd & goto EXIT )
+  call :CMD git svn rebase || ( popd & goto EXIT )
+  call :CMD git push origin master || ( popd & goto EXIT )
+)
+
+:EXIT
+set /A NEST_LVL-=1
+
+if %NEST_LVL% LEQ 0 pause
+
+exit /b
+
+:CMD
+echo.^>%*
+(%*)
+echo.
