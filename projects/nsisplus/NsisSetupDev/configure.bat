728c7e5133d459dca470a2195cbbfea302943ef1
andry81 <andry@inbox.ru>
2017-08-23 23:44:44 +0000

    * deploy scripts refactor
    git-svn-id: svn+ssh://svn.code.sf.net/p/nsisplus/deploy/trunk@15 493e4a90-db5d-4955-a802-ededfdaf30c0

---
+++ projects/nsisplus/NsisSetupDev/configure.bat
@@ -0,0 +1,22 @@
+@echo off
+
+setlocal
+
+if not defined NEST_LVL set NEST_LVL=0
+
+set /A NEST_LVL+=1
+
+(
+  echo.@echo off
+  echo.
+  echo.set "SVN.WCROOT_DIR=sf~NsisSetupDev"
+  echo.set "GIT.WCROOT_DIR=gh~nsisplus--NsisSetupDev"
+  echo.set "GIT2.WCROOT_DIR=bb~nsisplus--NsisSetupDev"
+  echo.
+) > "%~dp0configure.user.bat"
+
+set /A NEST_LVL-=1
+
+if %NEST_LVL% LEQ 0 pause
+
+exit /b
--- projects/nsisplus/configure.bat
+++ projects/nsisplus/configure.bat
@@ -1,22 +1,24 @@
 @echo off
 
+echo.^>%~dpnx0
+
 setlocal
+
+if not defined NEST_LVL set NEST_LVL=0
+
+set /A NEST_LVL+=1
 
 (
   echo.@echo off
   echo.
   echo.set PROJECT_NAME=nsisplus
-  echo.set "WCROOT_OFFSET=../../../_%%PROJECT_NAME%%"
+  echo.set "WCROOT_OFFSET=../../../../_%%PROJECT_NAME%%"
   echo.
   echo.rem primary mirror: github.com
   echo.set "GIT.REPO_OWNER=andry81"
-  echo.set "GIT.USER=user"
-  echo.set "GIT.EMAIL=user@mail.com"
   echo.
   echo.rem secondary mirror: bitbucket.org
   echo.set "GIT2.REPO_OWNER=andry81"
-  echo.set "GIT2.USER=user"
-  echo.set "GIT2.EMAIL=user@mail.com"
   echo.
   echo.set "NSISPLUS_DEPLOY.SVN.REPOROOT=https://svn.code.sf.net/p/nsisplus/deploy"
   echo.set "NSIS_SETUP_DEV.SVN.REPOROOT=https://svn.code.sf.net/p/nsisplus/NsisSetupDev"
@@ -34,3 +36,26 @@ setlocal
   echo.set "NSIS_SETUP_SAMPLES.GIT2.ORIGIN=https://%%GIT2.USER%%@bitbucket.org/%%GIT2.REPO_OWNER%%/nsisplus-nsissetupsamples.git"
   echo.
 ) > "%~dp0configure.user.bat"
+
+for /F "usebackq eol=	 tokens=* delims=" %%i in (`dir /A:D /B "%~dp0*.*"`) do (
+  set "DIR=%%i"
+  call :PROCESS_DIR
+)
+
+set /A NEST_LVL-=1
+
+if %NEST_LVL% LEQ 0 pause
+
+exit /b 0
+
+:PROCESS_DIR
+rem ignore directories beginning by `.`
+if "%DIR:~0,1%" == "." exit /b 0
+
+if exist "%~dp0%DIR%\configure.bat" call :CMD "%%~dp0%%DIR%%\configure.bat"
+
+exit /b
+
+:CMD
+echo.^>%*
+(%*)
